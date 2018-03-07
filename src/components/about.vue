1813366cc2c16d13369453e7462b007702402ed9
keithlem <keithlem9@gmail.com>
2018-03-07 16:45:56 -0500

    first commit

---
+++ src/components/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <p>Your Name:</p><input v-model="their_name">
+    <h1>Hello {{ their_name }}, This is an about page</h1>
+    <p>My name is Keith, I have been working in web development
+      for nearly 5 years and this is my Vue of life! Welcome!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+      their_name: 'You'
+    }
+  }
+}
+</script>
